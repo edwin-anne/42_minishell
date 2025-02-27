265c591ef9782c243e278478b80652843ffee62c
Lolqrdc <lola-rdc@outlook.fr>
2025-02-27 13:29:34 +0100

    beginning of builtin commands.

---
+++ include/builtin.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loribeir <loribeir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 13:15:35 by loribeir          #+#    #+#             */
+/*   Updated: 2025/02/27 13:16:55 by loribeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTIN_H
+# define BUILTIN_H
+
+# include "minishell.h"
+# include "../libft/include/libft.h" 
+# include "../libft/include/ft_printf.h"
+# include "../libft/include/get_next_line.h"
+
+
+#endif
---
+++ srcs/executing/builtin/pwd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loribeir <loribeir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 12:46:58 by loribeir          #+#    #+#             */
+/*   Updated: 2025/02/27 13:23:55 by loribeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+/**
+ * @brief : Print Working Directory. 
+ * The function should display the absolute path of the current directory.
+ * The command pwd has two options: -L and -P. 
+ */
+
+void    getcurrent_dir()
+{
+    
+}
+
+void    print_dir()
+{
+    
+}
