06886dde7b896dcfbd2f3e90415a8c2d44ddffa0
Edwin ANNE <edwin_anne@outlook.fr>
2025-03-22 16:41:16 +0100

    NORMINETTE

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: Edwin ANNE <eanne@student.42lehavre.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 21:42:03 by Edwin ANNE        #+#    #+#             */
-/*   Updated: 2025/03/21 11:08:15 by Edwin ANNE       ###   ########.fr       */
+/*   Updated: 2025/03/22 16:40:04 by Edwin ANNE       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,12 @@ void            append_str(char **result, const char *str);
 /*######	ENV_VAR_CASE 	######*/
 char            *ft_getpid();
 char            *ft_getexitcode();
+
+/*######	ENV_VAR_UTILS 	######*/
+char	        *get_env_value(t_env *env, char *key);
+int             is_var_char(char c);
+void            append_str(char **result, const char *str);
+int             update_quote_state(char c, int *in_sq, int *in_dq);
 
 /*######	UTILS 	######*/
 /*######	FT_SPLIT_MULTI  	######*/
--- srcs/parsing/env_var.c
+++ srcs/parsing/env_var.c
@@ -6,94 +6,77 @@
 /*   By: Edwin ANNE <eanne@student.42lehavre.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 10:14:46 by Edwin ANNE        #+#    #+#             */
-/*   Updated: 2025/03/22 12:03:19 by Edwin ANNE       ###   ########.fr       */
+/*   Updated: 2025/03/22 16:40:39 by Edwin ANNE       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-char	*get_env_value(t_env *env, const char *key)
+static int	handle_special_vars(char **res, char *str, int *i)
 {
-	while (env)
+	if (str[*i] == '?')
 	{
-		if (strcmp(env->key, key) == 0)
-			return (env->value);
-		env = env->next;
+		append_str(res, ft_getexitcode());
+		(*i)++;
+		return (1);
 	}
-	return (NULL);
-}
-
-static int	is_var_char(char c)
-{
-	if (isalnum(c))
+	if (str[*i] == '$')
+	{
+		append_str(res, ft_getpid());
+		(*i)++;
 		return (1);
-	if (c == '_')
-		return (1);
+	}
 	return (0);
 }
 
-void	append_str(char **result, const char *str)
+static void	handle_env_var(t_env *env, char **res, char *str, int *i)
 {
-	char	*new_result;
+	char	var_name[256];
+	char	*value;
+	int		j;
 
-	new_result = malloc(strlen(*result) + strlen(str) + 1);
-	if (!new_result)
-		return ;
-	ft_strlcpy(new_result, *result, ft_strlen(*result) + 1);
-	ft_strlcat(new_result, str, ft_strlen(*result) + ft_strlen(str) + 1);
-	free(*result);
-	*result = new_result;
+	j = 0;
+	while (str[*i] && is_var_char(str[*i]) && j < 255)
+		var_name[j++] = str[(*i)++];
+	var_name[j] = '\0';
+	value = get_env_value(env, var_name);
+	if (value)
+		append_str(res, value);
 }
 
-static char	*process_env_var(t_env *env, char *str)
+static void	handle_regular_char(char **res, char c)
+{
+	char	temp[2];
+
+	temp[0] = c;
+	temp[1] = '\0';
+	append_str(res, temp);
+}
+
+char	*process_env_var(t_env *env, char *str)
 {
 	char	*result;
-	char	*value;
-	char	var_name[256];
-	char	temp[2];
 	int		i;
-	int		j;
-	int		in_single_quotes;
-	int		in_double_quotes;
+	int		in_sq;
+	int		in_dq;
 
 	result = strdup("");
 	i = 0;
-	in_single_quotes = 0;
-	in_double_quotes = 0;
+	in_sq = 0;
+	in_dq = 0;
 	while (str[i])
 	{
-		if (str[i] == '\'' && !in_double_quotes)
-			in_single_quotes = !in_single_quotes;
-		else if (str[i] == '\"' && !in_single_quotes)
-			in_double_quotes = !in_double_quotes;
-		else if (str[i] == '$' && !in_single_quotes && str[i + 1])
+		if (update_quote_state(str[i], &in_sq, &in_dq))
+			i++;
+		else if (str[i] == '$' && !in_sq && str[i + 1])
 		{
 			i++;
-			if (str[i] == '?')
-			{
-				append_str(&result, ft_getexitcode());
-				i++;
+			if (handle_special_vars(&result, str, &i))
 				continue ;
-			}
-			if (str[i] == '$')
-			{
-				append_str(&result, ft_getpid());
-				i++;
-				continue ;
-			}
-			j = 0;
-			while (str[i] && is_var_char(str[i]) && j < 255)
-				var_name[j++] = str[i++];
-			var_name[j] = '\0';
-			value = get_env_value(env, var_name);
-			if (value)
-				append_str(&result, value);
-			continue ;
+			handle_env_var(env, &result, str, &i);
 		}
-		temp[0] = str[i];
-		temp[1] = '\0';
-		append_str(&result, temp);
-		i++;
+		else
+			handle_regular_char(&result, str[i++]);
 	}
 	return (result);
 }
@@ -105,8 +88,8 @@ void	execute_env_var(t_env *env, char **args)
 
 	i = 0;
 	if (!args)
-		return;
-	while(args[i])
+		return ;
+	while (args[i])
 	{
 		processed = process_env_var(env, args[i]);
 		free(args[i]);
