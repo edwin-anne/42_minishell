a8405a917069d6e8ef6e5f6184c2e0ec76fd3f8c
lolqrdc <lola-rdc@outlook.fr>
2025-03-22 17:31:24 +0100

    feat: handling the creation + execution of child processes for cmds exec.

--- srcs/executing/exec/child.c
+++ srcs/executing/exec/child.c
@@ -6,8 +6,60 @@
 /*   By: lolq <lolq@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 16:38:24 by lolq              #+#    #+#             */
-/*   Updated: 2025/03/19 16:38:41 by lolq             ###   ########.fr       */
+/*   Updated: 2025/03/22 17:23:24 by lolq             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "executing.h"
+
+/**
+ * @brief: handles the creation and execution of child processes for cmd exec.
+ *  - create child processes for each cmd in the linked list. 
+ *  - exec a cmd in a child process, handling if it's a built-ins or not. 
+ */
+
+int create_child(t_shell *shell, t_cmd *cmds)
+{
+    t_cmd   *tmp;
+    pid_t   pid;
+    
+    tmp = cmds;
+    while (tmp != NULL)
+    {
+        pid = fork();
+        if (pid < 0)
+            exit(1); // TODO: ne pas oublier de free la struct (edwin)
+        if (pid == 0)
+        {
+            // Child process
+            printf("Child process for command: %s\n", tmp->args[0]);
+            exec_child(shell);
+            exit(0); // Quit child after executing
+        }
+        // Parent continues
+        tmp->pid = pid;
+        printf("Parent created child pid: %d for cmd: %s\n", pid, tmp->args[0]);
+        tmp = tmp->next;
+    }
+    return (FAIL);
+}
+
+void exec_child(t_shell *shell)
+{
+    char    **env;
+
+    env = env_char(shell);
+    find_executable(shell->cmds, shell->env);
+    if (shell->cmds->is_builtin == true)
+        builtins_executing(shell, shell->cmds);
+    else 
+    {
+        if (execve(shell->cmds->path, shell->cmds->args, env) == -1)
+        {
+            perror("execve failed");
+            exit(EXIT_FAILURE);    
+        }
+    }
+    free_char_array(env);
+}
+
