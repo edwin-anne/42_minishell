bf57a7dbef942680eaa22af138f61df294586247
lolqrdc <lola-rdc@outlook.fr>
2025-03-22 12:18:08 +0100

    FIXED: echo with multiple -n is now working correctly.

--- srcs/executing/builtins/echo.c
+++ srcs/executing/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: lolq <lolq@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 10:02:15 by lolq              #+#    #+#             */
-/*   Updated: 2025/03/19 10:33:03 by lolq             ###   ########.fr       */
+/*   Updated: 2025/03/22 12:14:24 by lolq             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,10 @@ int   ft_echo(t_cmd *cmds)
     int     no_nl;
     int     i;
 
-    //(void)no_nl;
     no_nl = 0;
     i = 1;
     args = cmds->args;
-    if (args[i] && ft_strcmp(args[i], "-n") == 0)
+    while (args[i] && ft_strcmp(args[i], "-n") == 0)
     {
         no_nl = 1;
         i++;
@@ -36,10 +35,10 @@ int   ft_echo(t_cmd *cmds)
     {
         if (args[i + 1] == NULL)
             ft_fdprintf(1, "%s", args[i]);
-        else  
+        else
             ft_fdprintf(1, "%s ", args[i]);
         i++;
-    }  
+    }
     if (no_nl == 0)
         ft_fdprintf(1, "\n");
     return (SUCCESS);
